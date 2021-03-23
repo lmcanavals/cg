2bc66a8cad34b4240c2145ce20eb1fa6010c691a
Luis M Canaval S <43156214+lmcanavals@users.noreply.github.com>
2021-03-23 16:24:03 -0500

    Update triangle.cpp

--- src/ab-triangle/triangle.cpp
+++ src/ab-triangle/triangle.cpp
@@ -56,7 +56,7 @@ void main() {
     const char* fragmentShaderSrc = R"glsl(#version 330 core
 out vec4 color;
 void main() {
-    color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 })glsl";
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
