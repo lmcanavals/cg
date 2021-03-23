c9d485858da1977c9e8dcba112937b2b5dd9a1c3
Luis M Canaval S <public02@canaval.org>
2021-03-23 15:20:50 -0500

    First commit

---
+++ Makefile
@@ -0,0 +1,29 @@
+CC  := gcc
+CXX := g++
+CP  := cp
+RM  := rm -rf
+MD  := mkdir -p
+
+CXXFLAGS += -std=c++17 -pedantic -Wall -O3
+LDFLAGS += -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+
+CPPFLAGS += -Iinclude
+
+all: dirs bin/program
+
+%.: src/%/*.cpp dirs
+	$(CC) -c $(CPPFLAGS) -o build/glad.o src/glad.c
+	$(CXX) -c $(CXXFLAGS) $(CPPFLAGS) -o build/main.o $<
+	$(CXX) -o bin/program build/*.o $(LDFLAGS)
+	$(CP) $(shell dirname $<)/*.vert
+	$(CP) $(shell dirname $<)/*.frag
+
+dirs:
+	@echo Creating folders
+	@$(MD) build
+	@$(MD) bin
+
+clean:
+	@echo Removing build files
+	$(RM) build bin *.vert *.frag
+
---
+++ include/glutil.h
@@ -0,0 +1,99 @@
+#ifndef __GLUTIL_H__
+#define __GLUTIL_H__
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+GLFWwindow* glutilInit(int major, int minor,
+                       int width, int height,
+                       const char* title) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, title,
+                                          nullptr, nullptr);
+    if (window == nullptr) {
+        std::cerr << "Failed to create GLFW Window\n";
+        glfwTerminate();
+        return nullptr;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Not GLAD at all!\n";
+        return nullptr;
+    }
+
+    return window;
+}
+
+class Shader {
+    unsigned int program;
+
+    int ok;             // check for error status
+    char infoLog[512];  // get error status info
+
+public:
+    Shader(std::string vertexFileName = "shader.vert",
+           std::string fragmentFileName = "shader.frag") {
+        std::ifstream vertexFile(vertexFileName);
+        std::string vertexSrc;
+        std::getline(vertexFile, vertexSrc, '\0');
+
+        std::ifstream fragmentFile(fragmentFileName);
+        std::string fragmentSrc;
+        std::getline(fragmentFile, fragmentSrc, '\0');
+
+        GLuint vertex = mkShader(vertexSrc.c_str(), GL_VERTEX_SHADER);
+        GLuint fragment = mkShader(fragmentSrc.c_str(), GL_FRAGMENT_SHADER);
+
+        program = glCreateProgram();
+        glAttachShader(program, vertex);
+        glAttachShader(program, fragment);
+        glLinkProgram(program);
+        glGetProgramiv(program, GL_LINK_STATUS, &ok);
+        if (!ok) {
+            glGetProgramInfoLog(program, 512, nullptr, infoLog);
+            std::cout << "Error::shader::program::link_failed\n"
+                << infoLog << std::endl;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    ~Shader() {
+        glDeleteProgram(program);
+        glfwTerminate();
+    }
+    void useProgram() {
+        glUseProgram(program);
+    }
+
+private:
+    GLuint mkShader(const char* source, GLenum type) {
+        GLuint shader = glCreateShader(type);
+        glShaderSource(shader, 1, &source, nullptr);
+        glCompileShader(shader);
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+        if (!ok) {
+            glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+            std::cerr << "Error::shader::compilation_failed\n"
+                << infoLog << std::endl;
+            return 0;
+        }
+        return shader;
+    }
+};
+
+#endif
